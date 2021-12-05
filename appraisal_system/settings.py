ac4e45280ad560c963f6bc778139aadb34b14a49
Sarim <sarimshujaat@gmail.com>
2021-12-06 05:01:14 +0530

    installed initial apps

--- appraisal_system/settings.py
+++ appraisal_system/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user',
+    'student',
+    'teacher',
+
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
