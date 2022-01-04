b497020e1a2da3211c3200b1e4ad7bc9622dda2e
Sarim <sarimshujaat@gmail.com>
2022-01-05 02:59:15 +0530

    Initial model setup for attendance, class and ratings

--- course/models.py
+++ course/models.py
@@ -4,6 +4,37 @@ class Course(models.Model):
 
     course_name = models.CharField(max_length=70)
     department = models.ForeignKey('department.Department', on_delete = models.CASCADE)
+    for_semester = models.IntegerField(blank= True, null= True)
 
     def __str__(self):
         return self.name + f" - {self.department.department_name} Department"
+
+
+
+class CourseClass(models.Model):
+
+    course = models.ForeignKey('course.Course', blank= True, null= True, on_delete= models.SET_NULL)
+    teacher = models.ManyToManyField('teacher.Teacher', blank= True, null= True)
+    section = models.CharField(max_length=1, blank=True, null= True)
+    students = models.ManyToManyField('student.Student', blank= True, null= True)
+
+    def __str__(self):
+        return self.course + f" - Section {self.section} - {self.teacher}"
+
+
+class Attendance(models.Model):
+
+    date = models.DateTimeField()
+    subject = models.OneToOneField('course.CourseClass', blank= True, null= True, on_delete= models.SET_NULL)
+    student = models.OneToOneField('student.Student', blank= True, null= True, on_delete= models.SET_NULL)  
+    # when creating the view for adding attendance, we need to check that only the students who are in a particular
+    # course are selected for the attendance  
+
+
+    # also, add semester and section fields to student model
+
+
+class Ratings(models.Model):
+
+    student = models.ForeignKey('student.Student', blank= True, null= True, on_delete= models.SET_NULL)
+    teacher = models.ForeignKey('teacher.Teacher', blank= True, null= True, on_delete= models.SET_NULL)
