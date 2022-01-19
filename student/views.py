ea5d42835195830b542749e6cd0b6b85825700bf
Sarim <sarimshujaat@gmail.com>
2022-01-19 14:09:03 +0530

    Modified student detail view

--- student/views.py
+++ student/views.py
@@ -52,7 +52,7 @@ class StudentCreateView(CreateView):
 
 
 
-from course.models import CourseClass
+from course.models import CourseClass, Attendance
 
 class StudentDetailView(DetailView):
 
@@ -67,8 +67,38 @@ class StudentDetailView(DetailView):
     def get_context_data(self, *args, **kwargs):
         context = super(StudentDetailView, self).get_context_data(*args, **kwargs)
 
-        student_classes = CourseClass.objects.filter(students__id = self.object.id).order_by('course')
+        student_classes = CourseClass.objects.filter(students__id = self.object.id).order_by('course').all()
         context['classes'] = student_classes
+
+        classes_dict = student_classes.values()
+        print(classes_dict)
+
+        for cls in classes_dict:
+            attendance = Attendance.objects.filter(subject = cls['id']).filter(student = self.object.id)
+            print(attendance)
+            atten_received = 0
+            total_atten = 0
+            for entry in attendance:
+                atten_received += entry.no_of_attendances_granted
+                total_atten += entry.no_of_attendances_possible
+
+            cls['attendance_received'] = atten_received
+            cls['total_attendance'] = total_atten
+
+            try:
+                atten_percentage = round((atten_received/total_atten)*100, 2)
+            except Exception as e:
+                atten_percentage = 0
+
+            cls['attendance_percentage'] = atten_percentage
+
+            if atten_percentage >= 60:
+                cls['eligibility'] = "Eligible"
+            else:
+                cls['eligibility'] = "Not Eligible"
+
+
+        context['classes_dict'] = classes_dict
 
         return context
 
@@ -85,3 +115,4 @@ class StudentUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('students:student-detail-view', args = (self.object.id,))
+
